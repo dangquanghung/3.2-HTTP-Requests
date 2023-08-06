fb8ac6800a7f0c64e9b5aea75493f3fac372eeed
dangquanghung <43923266+dangquanghung@users.noreply.github.com>
2023-08-06 23:01:46 +0700

    add more endpoint

--- index.js
+++ index.js
@@ -7,13 +7,29 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-  res.send("<h1>hello</h1>")
+  res.send("<h1>hello</h1>");
 });
 
 app.get("/about", (req, res) => {
-  res.send("<h1>Hi, this is about me</h1>")
-})
+  res.send("<h1>Hi, this is about me</h1>");
+});
+
+app.post("/register", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.put("/user/hungdq", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.patch("/user/hungdq", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.delete("/user/hungdq", (req, res) => {
+  res.sendStatus(200);
+});
 
 app.get("/contact", (req, res) => {
-  res.send("<h1>call me if you ... <br>are sad: 0332812366 </h1>")
-})
+  res.send("<h1>call me if you ... <br>are sad: 0332812366 </h1>");
+});
