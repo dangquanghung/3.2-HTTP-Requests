74b0d4c450d7ef2d8758b9f64db7ab4e62609282
dangquanghung <43923266+dangquanghung@users.noreply.github.com>
2023-08-14 23:48:00 +0700

    update parser body and html file

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
+import bodyParser from "body-parser";
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.listen(port, () => {
   console.log(`Server run at port ${port}`);
 });
 
 app.get("/", (req, res) => {
-  res.send("<h1>hello</h1>");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.get("/about", (req, res) => {
   res.send("<h1>Hi, this is about me</h1>");
 });
 
-app.post("/register", (req, res) => {
-  res.sendStatus(200);
+app.post("/submit", (req, res) => {
+  console.log(req.body);
 });
 
 app.put("/user/hungdq", (req, res) => {
