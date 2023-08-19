c1d03abb3a8a4b5c4e9df1cfcf7a7c77b9de7096
dangquanghung <43923266+dangquanghung@users.noreply.github.com>
2023-08-20 02:50:19 +0700

    new hot code

--- index.js
+++ index.js
@@ -4,38 +4,39 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000;
+
+var userIsAuthorised = false;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.listen(port, () => {
-  console.log(`Server run at port ${port}`);
-});
+function passwordCheck(req, res, next) {
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    userIsAuthorised = true;
+  }
+  next();
+}
+
+app.use(passwordCheck);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
+app.post("/check", (req, res) => {
+  if (userIsAuthorised) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+  }
+  //Alternatively res.redirect("/")
+});
+
+app.listen(port, () => {
+  console.log(`Server run at port ${port}`);
+});
 app.get("/about", (req, res) => {
   res.send("<h1>Hi, this is about me</h1>");
-});
-
-app.post("/submit", (req, res) => {
-  console.log(req.body);
-});
-
-app.put("/user/hungdq", (req, res) => {
-  res.sendStatus(200);
-});
-
-app.patch("/user/hungdq", (req, res) => {
-  res.sendStatus(200);
-});
-
-app.delete("/user/hungdq", (req, res) => {
-  res.sendStatus(200);
-});
-
-app.get("/contact", (req, res) => {
-  res.send("<h1>call me if you ... <br>are sad: 0332812366 </h1>");
 });
